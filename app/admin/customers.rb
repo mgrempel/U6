30b9f484f07d9c3fc12a0ef24ddf0d10bad1123a
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-11-07 12:58:22 -0600

    Forgot to configure ActiveStorage

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,16 +1,12 @@
 ActiveAdmin.register Customer do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :full_name, :phone_number, :email_address, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email_address, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
